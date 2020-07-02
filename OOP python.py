50eb0fd2b47260ba0fb7c3266ca137a876187c94
S1lent1 <55151335+S1lent1@users.noreply.github.com>
2020-07-02 21:45:04 +0300

    Add files via upload

---
+++ OOP python.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractclassmethod
+#нужно создать класс утка(с характерными методами) так, чтобы небыло ошибок если создастся утка, не использующая
+# те или иные методы
+# идея в том, что метод полета и крякания вынесены в отдельные абстрактные классы, от которых наследуется класс утка
+# (от которой наследуются виды и типы уток)
+# вопрос: как объявить метод, но не реализовать
+#
+
+# class Flyable(metaclass= ABCMeta):
+#
+#     def flybywings(self):
+#
+#
+#     def flybymech(self):
+#
+#
+#     def anflyable(self):
+#
+# class Quackable():
+#     def quack(self):
+#         print("*КРяя*КряяЯ*")
+#     def anquackable(self):
+#         print("*и треск сверчков*")
+
+
+
+class Duck(ABC):#Flyable, Quackable):
+
+    def __init__(self, flying=0, quacking=0):
+        self.flying = flying
+        self.quacking = quacking
+
+    def Flyable(self):
+        if self.flying == 1:
+            print('it can fly')
+
+    def Quackable(self):
+        if self.quacking == 1:
+            print('it can quack')
+#нельзя создавать объекты, пока ОБА метода не переопределены
+class MallardDuck(Duck):
+    def __init__(self):
+        Duck.__init__(self)
+#нельзя создавать объекты, пока ОБА метода не переопределены
+class REdHeadDuck(Duck):
+    def __init__(self):
+        Duck.__init__(self)
+#нельзя создавать объекты, пока ОБА метода не переопределены
+class RubberDuck(Duck):
+    def __init__(self):
+        Duck.__init__(self)
+        self.flying = 0
+#нельзя создавать объекты, пока ОБА метода не переопределены
+class DecoyDuck(Duck):
+     def __init__(self, flying, quacking):
+         Duck.__init__(self)
+         self.flying = flying
+         self.quacking = quacking
+
+oleg = DecoyDuck()
+mald = MallardDuck()
+mald.quacking = 1
+mald.flying = 1
+
